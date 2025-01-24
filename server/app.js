a6b911838131985e4ded72a9201466c1d41ec711
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2025-01-24 17:56:11 +0530

    update

--- server/app.js
+++ server/app.js
@@ -36,19 +36,13 @@ app.use(cookieParser());
 
 // CORS setup
 const allowedOrigins = [
-  "https://id-card-ten.vercel.app",
-  "http://localhost:3000",
-  "https://id-card-bvxf-8tw7c7o4g-arpits-projects-1c6b9bf9.vercel.app",
-  "https://id-card-bvxf.vercel.app",
-  "https://id-card-pro.vercel.app/",
-  "http://www.cardpro.co.in/",
-  "https://www.cardpro.co.in",
-  "https://cardpro.co.in",
+ 
+  
 ];
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: "*",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization", "Content-Disposition"],
     exposedHeaders: ["Content-Disposition"], // Include this
