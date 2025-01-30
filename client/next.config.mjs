e09929b845302d65d7f882c6f61afcde8cd3928d
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2025-01-30 16:22:39 +0530

    yu

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -40,18 +40,16 @@ const nextConfig = {
           },
           {
             key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.gstatic.com;
-              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-              img-src 'self' data: blob: https://www.gstatic.com;
-              connect-src 'self' https://api.cardpro.co.in/;
-              frame-src 'self';
-              font-src 'self' https://fonts.gstatic.com;
-              object-src 'none';
-              media-src 'self' blob: https://www.gstatic.com;
-              worker-src 'self' blob:;
-            `,
+            value: `default-src 'self';
+                    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://www.gstatic.com;
+                    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+                    img-src 'self' data: blob: https://www.gstatic.com;
+                    connect-src 'self' https://api.cardpro.co.in/;
+                    frame-src 'self';
+                    font-src 'self' https://fonts.gstatic.com;
+                    object-src 'none';
+                    media-src 'self' blob: https://www.gstatic.com;
+                    worker-src 'self' blob:;`, // Fixed formatting here
           },
         ],
       },
